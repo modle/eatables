63b85b82bbe1403ad156e20c41969f5c6806dd5f
modle <odle.matthew@gmail.com>
2015-08-01 17:59:53 -0500

    styles, attempt 6

--- eatables/settings.py
+++ eatables/settings.py
@@ -16,7 +16,7 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
+STATIC_URL = '/eatables/staticfiles/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
