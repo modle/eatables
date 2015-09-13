73d8ad49b6983975f6beb7a16713820ddbe25966
modle <odle.matthew@gmail.com>
2015-09-13 10:07:53 -0500

    added Category to admin models

--- menu/admin.py
+++ menu/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Recipe, Ingredient, ShoppingList, IngredientMaster, Comment
+from .models import Recipe, Ingredient, ShoppingList, IngredientMaster, Comment, Category, Fridge
 
 
 admin.site.register(Recipe)
@@ -9,3 +9,5 @@ admin.site.register(Ingredient)
 admin.site.register(ShoppingList)
 admin.site.register(IngredientMaster)
 admin.site.register(Comment)
+admin.site.register(Category)
+admin.site.register(Fridge)
