7155a7d43b0dd9f3f32f05c4efbaa5c591363287
modle <odle.matthew@gmail.com>
2015-09-15 22:46:32 -0500

    playing with rating

---
+++ eatables/static/bootstrap/js/eatables.js
@@ -0,0 +1,22 @@
+
+
+function Rate() {
+    alert("ya clicked it")
+};
+
+
+function rate1() {
+    alert("1 star!")
+};
+function rate2() {
+    alert("2 stars!")
+};
+function rate3() {
+    alert("3 stars!")
+};
+function rate4() {
+    alert("4 stars!")
+};
+function rate5() {
+    alert("5 stars!")
+};
