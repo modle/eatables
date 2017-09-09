d6354efbd924fce14e3ad9405210ccee9552e55a
Matthew Odle <matt.odle@tdstelecom.com>
2017-09-09 15:11:33 -0500

    fix line endings; add eslint config; add npm config

--- eatables/static/menu/js/main.js
+++ eatables/static/menu/js/main.js
@@ -2,46 +2,46 @@ $(document).ready( function() {
   $('.notSelectable').disableSelection();
 });
 
-
-$(".recipe_block").mousemove(function(){
+/* eslint-disable camelcase*/
+$('.recipe_block').mousemove(function() {
     recipe_id = this.id;
-})
-$(".recipe_block").swipe({
-  tap:function(event, target) {
-    window.location.href = "/"+recipe_id+"/recipe_details/";
-  },
-  swipeLeft:function(event, direction, distance, duration, fingerCount) {
-    window.location.href = "/"+recipe_id+"/edit_recipe/";
-  },
-  threshold:50,
-  allowPageScroll:"auto"
 });
+$('.recipe_block').swipe({
+  tap: function(event, target) {
+    window.location.href = '/'+recipe_id+'/recipe_details/';
+  },
+  swipeLeft: function(event, direction, distance, duration, fingerCount) {
+    window.location.href = '/'+recipe_id+'/edit_recipe/';
+  },
+  threshold: 50,
+  allowPageScroll: 'auto',
+});
+/* eslint-enable camelcase*/
 
 
-
-//makes text non-selectable on mobile, and prevents the highlight on long-press function
+// makes text non-selectable on mobile, and prevents the highlight on long-press function
 $.fn.extend({
     disableSelection: function() {
         this.each(function() {
             this.onselectstart = function() {
                 return false;
             };
-            this.unselectable = "on";
+            this.unselectable = 'on';
             $(this).css('-moz-user-select', 'none');
             $(this).css('-webkit-user-select', 'none');
         });
         return this;
-    }
+    },
 });
 
 
 // This function gets cookie with a given name
 function getCookie(name) {
-    var cookieValue = null;
+    let cookieValue = null;
     if (document.cookie && document.cookie != '') {
-        var cookies = document.cookie.split(';');
-        for (var i = 0; i < cookies.length; i++) {
-            var cookie = jQuery.trim(cookies[i]);
+        let cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            let cookie = jQuery.trim(cookies[i]);
             // Does this cookie string begin with the name we want?
             if (cookie.substring(0, name.length + 1) == (name + '=')) {
                 cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
@@ -51,7 +51,7 @@ function getCookie(name) {
     }
     return cookieValue;
 }
-var csrftoken = getCookie('csrftoken');
+let csrftoken = getCookie('csrftoken');
 
 
 /*
@@ -65,13 +65,13 @@ function csrfSafeMethod(method) {
 function sameOrigin(url) {
     // test that a given url is a same-origin URL
     // url could be relative or scheme relative or absolute
-    var host = document.location.host; // host + port
-    var protocol = document.location.protocol;
-    var sr_origin = '//' + host;
-    var origin = protocol + sr_origin;
+    let host = document.location.host; // host + port
+    let protocol = document.location.protocol;
+    let srOrigin = '//' + host;
+    let origin = protocol + srOrigin;
     // Allow absolute or scheme relative URLs to same origin
     return (url == origin || url.slice(0, origin.length + 1) == origin + '/') ||
-        (url == sr_origin || url.slice(0, sr_origin.length + 1) == sr_origin + '/') ||
+        (url == srOrigin || url.slice(0, srOrigin.length + 1) == srOrigin + '/') ||
         // or any other URL that isn't scheme relative or absolute i.e relative.
         !(/^(\/\/|http:|https:).*/.test(url));
 }
@@ -82,7 +82,7 @@ $.ajaxSetup({
             // Send the token to same-origin, relative URLs only.
             // Send the token only if the method warrants CSRF protection
             // Using the CSRFToken value acquired earlier
-            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+            xhr.setRequestHeader('X-CSRFToken', csrftoken);
         }
-    }
+    },
 });
